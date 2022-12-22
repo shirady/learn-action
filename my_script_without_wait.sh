7b3a3783de79ee52241ac6217c7e5a4e224c6b53
shirady <57721533+shirady@users.noreply.github.com>
2022-12-22 18:32:21 +0200

    wip

--- my_script_without_wait.sh
+++ my_script_without_wait.sh
@@ -18,23 +18,3 @@ fi
 
 # source:
 # https://stackoverflow.com/questions/55073453/wait-for-kubernetes-job-to-complete-on-either-failure-success-using-command-line
-#!/bin/bash
-set -x
-
-retval_complete=1
-retval_failed=1
-while [[ $retval_complete -ne 0 ]] && [[ $retval_failed -ne 0 ]]; do
-  sleep 5
-  output=$(kubectl wait --for=condition=failed job/job-name --timeout=0 2>&1)
-  retval_failed=$?
-  output=$(kubectl wait --for=condition=complete job/job-name --timeout=0 2>&1)
-  retval_complete=$?
-done
-
-if [ $retval_failed -eq 0 ]; then
-    echo "Job failed. Please check logs."
-    exit 1
-fi
-
-# source:
-# https://stackoverflow.com/questions/55073453/wait-for-kubernetes-job-to-complete-on-either-failure-success-using-command-line
